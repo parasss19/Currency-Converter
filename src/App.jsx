010b4b06e07b14ba2ea433b7c0c3627b10e915d3
Paras <mehtaparas1901@gmail.com>
2023-10-22 16:54:01 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import { useState } from 'react'
+
+import InputBox from './Components/InputBox'
+import UseCurrencyInfo from './CustomHook/UseCurrenyInfo'
+import bgImage from './assets/bg1.jpg'
+
+function App() {
+  const [amount, setAmount] = useState("")
+  const [currencyFrom, setCurrencyFrom] = useState("usd")
+  const [currencyTo, setCurrencyTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState("")
+
+  //use our custom hook UseCurrencyInfo and currencyInfo contain the data return by our custom hook
+  const currencyInfo = UseCurrencyInfo(currencyFrom)    
+
+  //now the fetched object keys are currency type (like usd,inr etc) which we want to show to users ... so to get all the keys we use Object.keys()
+  const options = Object.keys(currencyInfo)
+
+  //swap button functionality
+  const swap = () =>{
+    setAmount(convertedAmount)
+    setCurrencyFrom(currencyTo)
+    setCurrencyTo(currencyFrom)
+    setConvertedAmount(amount)
+  }
+
+  //Now the main conversion button functionality which show the result 
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[currencyTo])
+  }
+
+  return (
+    <div className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"  style={{backgroundImage:`url(${bgImage})`, backgroundSize:'cover'}}>
+    <div className="w-full">
+
+        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+            <form onSubmit={(e) => { 
+              e.preventDefault()
+              convert()
+            }  }  >
+
+            {/* First input */}
+              <div className="w-full mb-1">
+                  <InputBox 
+                  label="From" 
+                  amount={amount}
+                  currencyOptions={options}
+                  onCurrencyChange={(currency) => setCurrencyFrom(currency)}
+                  onAmountChange={(amount) => setAmount(amount)}
+                  selectCurrency={currencyFrom}
+                  />
+              </div>
+
+            {/* Swap button */}
+              <div className="relative w-full h-0.5">
+                    <button
+                        type="button"
+                        className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5" 
+                        onClick={swap}
+                    >
+                       swap
+                    </button>
+              </div>
+            
+            {/* Second input */}
+              <div className="w-full mt-1 mb-4">
+                    <InputBox 
+                    label="To"
+                    amount={convertedAmount}
+                    // onAmountChange={(amount) => setAmount(amount)}
+                    currencyOptions={options}
+                    onCurrencyChange={(currency)=> setCurrencyTo(currency)}
+                    selectCurrency={currencyTo}
+                     />  
+              </div>
+
+            {/* Convert Button */}
+              <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                        Convert {currencyFrom.toUpperCase()} to {currencyTo.toUpperCase()}
+              </button>
+            </form>
+
+        </div>
+    </div>
+</div>
+
+  )
+}
+
+export default App
+
+
+  
---
+++ src/Components/InputBox.jsx
@@ -0,0 +1,60 @@
+import React, {useId} from 'react'
+
+function InputBox({
+    //We pass object with some values which we want from the users as func parameter
+    label,                      //used to define label either "from" or "to"
+    amount,                     //used to get amount from the user
+    onAmountChange,             //update state when amount is changed (jab amount change hoga toh humko state update krni hogi jo humne app.jsx m banai h )
+    selectCurrency = "usd" ,    //by default usd and user select currency type
+    onCurrencyChange,           //same for onCurrencyChange 
+    currencyOptions = [],       //it is used to store diff types of curr we fetch and by default for saftey we pass blank array so that our app not crash
+       
+}) 
+  {
+    
+    //useId hook will genrate unique id which can be used to bind things together 
+    const amountInputId = useId()   
+    
+    return (
+        <div className='flex p-3 rounded-lg text-sm  bg-white'>
+            
+            {/* Label and Input(left side) */}
+            <div className='w-1/2 flex flex-wrap p-3'>
+               <label htmlFor={amountInputId} className='text-black/40 text-xl text-left  w-full'>
+                {label}
+               </label>
+
+               <input 
+               id ={amountInputId}
+               className='outline-none bg-slate-100 rounded-lg p-2'
+               type="number"
+               placeholder='Amount'
+               value={amount}
+               onChange={(e)=> onAmountChange && onAmountChange(Number(e.target.value))}     //js many times take e.target.value as string  so for safty we convert into number
+               />
+            </div>
+
+            {/* CurrencyType (Right side) */}
+            <div className = 'flex flex-wrap justify-end text-right p-3 w-1/2 '>
+               <p className='text-xl text-black/40 w-full mb-8'>Currency Type</p>
+               
+               <select className='px-4 py-2 outline-none text-lg bg-blue-50 rounded-lg'
+                 value={selectCurrency}
+                 onChange = {(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+                 >
+                
+                {/* we use map to get all the currency inside  currencyOptions array*/}
+                {currencyOptions.map((currency)=> (
+                <option key={currency} value={currency} >
+                  {currency}
+                </option>
+                ))}
+               </select>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default InputBox;
